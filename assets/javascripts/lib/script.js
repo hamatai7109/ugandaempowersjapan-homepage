32e1e9134091855032ec5764309a009069360ae5
hamatai <taishihamano0515@gmail.com>
2024-01-20 13:10:25 +0900

    Complete about section

--- assets/javascripts/lib/script.js
+++ assets/javascripts/lib/script.js
@@ -2,3 +2,12 @@
 document.getElementById("hamburgerBtn").onclick = function () {
   document.getElementById("header").classList.toggle("isHeaderOpen");
 };
+
+// slick（aboutのスライダー）.about__slider
+$(".about__slider").slick({
+  infinite: true,
+  dots: true,
+  arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+});
